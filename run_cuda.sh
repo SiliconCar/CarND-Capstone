68456c62259b47f856db4936989c9538666d73dc
Thomas Antony <tantony@purdue.edu>
2017-10-03 01:23:21 -0500

    Added GUI capapbility to CUDA docker script.

--- run_cuda.sh
+++ run_cuda.sh
@@ -8,7 +8,7 @@ CONTAINER_NAME="udacity_carnd"
 
 if [ "$(docker ps -a | grep ${CONTAINER_NAME})" ]; then
   echo "Attaching to running container..."
-  nvidia-docker exec -it ${CONTAINER_NAME} bash $@
+  nvidia-docker exec -eDISPLAY=$DISPLAY -it ${CONTAINER_NAME} bash $@
 else
-  nvidia-docker run --name ${CONTAINER_NAME} --rm -it -p 4567:4567 -v "${UDACITY_SOURCE}:/udacity" ${UDACITY_IMAGE} $@
+  nvidia-docker run --name ${CONTAINER_NAME} --rm -it -p 4567:4567 -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v "${UDACITY_SOURCE}:/udacity" ${UDACITY_IMAGE} $@
 fi
