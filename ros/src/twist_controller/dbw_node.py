4d97e67a232150ec1185d0d2b0de195b8a75eb45
sean <searn@ualberta.ca>
2017-11-08 09:34:28 -0700

    change dbw rate to 50 HZ

--- ros/src/twist_controller/dbw_node.py
+++ ros/src/twist_controller/dbw_node.py
@@ -83,7 +83,7 @@ class DBWNode(object):
 
 
     def loop(self):
-        rate = rospy.Rate(10) # 50Hz
+        rate = rospy.Rate(50) # 50Hz
         while not rospy.is_shutdown():
             # TODO: Get predicted throttle, brake, and steering using `twist_controller`
             # You should only publish the control commands if dbw is enabled
